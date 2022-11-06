badaf800fc5a6f1592d6cfedb7f855ac6e8435ea
LJ <lajeshwarlimbu@gmail.com>
2022-11-06 16:51:30 +0000

    particle project

--- script.js
+++ script.js
@@ -8,14 +8,32 @@ window.addEventListener('load',function(){
 
    
     class Particle{
-        constructor(ParticleEffectMaster){
+        constructor(ParticleEffectMaster,x,y,color){
             this.effect = ParticleEffectMaster;
-            this.x = Math.random() *  this.effect.width;
-            this.y = Math.random() *  this.effect.height;
-            this.size = 30;
+            this.x = Math.random() * this.effect.width;
+            this.y =  Math.random() * this.effect.height;
+            this.originX = Math.floor(x);
+            this.originY = Math.floor(y);
+            this.color = color;
+            this.size = this.effect.gap;
+            this.vx = 0;
+            this.vy = 0;
+            this.ease = 0.15;
         }
         draw(context){
+            context.fillStyle = this.color;
             context.fillRect(this.x,this.y,this.size,this.size);
+        }
+        update(){
+            this.x += (this.originX - this.x) *this.ease;
+            this.y += (this.originY - this.y)*this.ease;
+
+        }
+        warp(){
+            this.x = Math.random() * this.effect.width;
+            this.y =  Math.random() * this.effect.height;
+            this.ease = 0.05;
+
         }
     }
 
@@ -28,24 +46,57 @@ window.addEventListener('load',function(){
             this.centerX = this.width *0.5;
             this.centerY = this.height*0.5;
             this.x = this.centerX - this.image.width *0.5;
+            this.gap = 5;
         }
-        init(){
-            for(let i =0; i<10; i++){
-            this.particles.push(new Particle(this));          
-            }  
+        init(context){
+            context.drawImage(this.image,this.x,0);
+            const pixels = context.getImageData(0,0,this.width,this.height).data; 
+            for(let y= 0; y<this.height; y+=this.gap){
+                for(let x= 0; x<this.width; x+=this.gap){
+                    const index = (y*this.width + x ) * 4;
+                    const red = pixels[index];
+                    const green = pixels[index+1];
+                    const blue = pixels[index+2];
+                    const alpha = pixels[index+3]
+                    const color = 'rgb('+red+','+green+','+blue+')';
+                    
+                    if(alpha > 0){
+                        this.particles.push(new Particle(this,x,y,color));
+                    }
+                
+                }
+            }
         }
         draw(context){
             this.particles.forEach(particle =>particle.draw(context));
-            context.drawImage(this.image,this.x,0);
+           
+        }
+        update(){
+             this.particles.forEach(particle =>particle.update());
+           
+
+        }
+        warp(){
+            this.particles.forEach(particle =>particle.warp());
         }
     }
 
     const effect = new ParticleEffectMaster(canvas.width, canvas.height);
-    effect.init();
-    effect.draw(ctx);
+    effect.init(ctx);
+    console.log(effect)
+ 
     function animate(){
-
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        effect.draw(ctx);
+        effect.update();
+        requestAnimationFrame(animate);
     }
-  
+     animate();
+
+     const warpButton = this.document.getElementById('warpButton'
+     );
+     warpButton.addEventListener('click',function(){
+        effect.warp();
+     });
 });
 
